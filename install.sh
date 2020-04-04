bed532d73aff9ec40743e15bc0bd2eb4ccf9fb06
nicosommi <nico.sommi@sumup.com>
2020-04-04 14:55:18 +0200

    Adjust paths

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
-printf "${BLUE}Looking for an existing zsh config...${NORMAL}\n"
-if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-  printf "${YELLOW}Found ~/.zshrc.${NORMAL} ${GREEN}Backing up to ~/.zshrc.pre-oh-my-zsh${NORMAL}\n";
-  mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
-fi
-
-printf "${BLUE}Using the Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
-cp "$ZSH"/templates/zshrc.zsh-template ~/.zshrc
+printf "${BLUE}Installing nicosommi dotfiles...${NORMAL}\n"
+printf "${BLUE}Using nicosommi Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
+cp ~/dotfiles/zsh/templates/zshrc.zsh-template ~/.zshrc
 sed "/^export ZSH=/ c\\
 export ZSH=\"$ZSH\"
 " ~/.zshrc > ~/.zshrc-omztemp
