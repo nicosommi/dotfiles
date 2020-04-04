0adcabd253f765ff4897c89f28f0226bfcd2656f
nicosommi <nico.sommi@sumup.com>
2020-04-04 15:17:31 +0200

    Remove mv

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 printf "${BLUE}Installing nicosommi dotfiles...${NORMAL}\n"
 printf "${BLUE}Using nicosommi Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
 cp ~/dotfiles/zsh/templates/zshrc.zsh-template ~/.zshrc
-mv -f ~/.zshrc-omztemp ~/.zshrc
