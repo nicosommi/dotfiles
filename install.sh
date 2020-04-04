8150cf3d7ace46dc3e12c8f68f60c8516fc618b5
nicosommi <nico.sommi@sumup.com>
2020-04-04 15:15:58 +0200

    Don't replace env ZSH

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 printf "${BLUE}Installing nicosommi dotfiles...${NORMAL}\n"
 printf "${BLUE}Using nicosommi Oh My Zsh template file and adding it to ~/.zshrc${NORMAL}\n"
 cp ~/dotfiles/zsh/templates/zshrc.zsh-template ~/.zshrc
-sed "/^export ZSH=/ c\\
-export ZSH=\"$ZSH\"
-" ~/.zshrc > ~/.zshrc-omztemp
 mv -f ~/.zshrc-omztemp ~/.zshrc
