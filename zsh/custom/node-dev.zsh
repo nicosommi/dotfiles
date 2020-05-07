e55991443c75e79fae24481c9c81c66c02adb277
nicosommi <nico.sommi@sumup.com>
2020-05-07 11:55:10 +0200

    Refresh nvm

--- zsh/custom/node-dev.zsh
+++ zsh/custom/node-dev.zsh
@@ -1,3 +1,7 @@
 install-nvm() {
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | zsh
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | zsh
+}
+
+nvm-set-default-latest() {
+  nvm alias default node
 }
