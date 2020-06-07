bd02a9c0beb9ec55b96746b61c92e1261121b4d4
nicosommi <nico.sommi@sumup.com>
2020-06-07 09:30:16 +0200

    make alias a var

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -1,2 +1,2 @@
-alias clouddrive=~/Library/Mobile\ Documents/com\~apple\~CloudDocs
+clouddrive="~/Library/Mobile\ Documents/com\~apple\~CloudDocs"
 alias markt="/Applications/Mark\ Text.app/Contents/MacOS/Mark\ Text"
