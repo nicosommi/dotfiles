8128d768ef719377445bfa376e4418b95e8fa99e
nicosommi <nicosommi@gmail.com>
2021-08-03 15:01:05 +0200

    Fixes ln install by not symlinking folders but files

--- install-ln.sh
+++ install-ln.sh
@@ -6,9 +6,10 @@ ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # Git hooks templates (ctags)
 git config --global init.templatedir '~/.git_template'
-mkdir -p ~/.git_template
+mkdir -p ~/.git_template/{hooks,info}
 # cp -R git_templates/* ~/.git_template
-ln -sf ~/dotfiles/git_templates ~/.git_template
+ln -sf ~/dotfiles/git_templates/hooks/* ~/.git_template/hooks/.
+ln -sf ~/dotfiles/git_templates/info/* ~/.git_template/info/.
 # Enables git ctags command
 git config --global alias.ctags '!.git/hooks/ctags'
 
@@ -18,9 +19,13 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 printf "${BLUE}Configuring fish${NORMAL}\n"
 
-mkdir -p ~/.config
+mkdir -p ~/.config/fish/{completions,conf.d,functions}
 
 # cp -R ~/dotfiles/fish ~/.config
-ln -sf ~/dotfiles/fish ~/.config/fish
+ln -sf ~/dotfiles/fish/completions/*.fish ~/.config/fish/completions
+ln -sf ~/dotfiles/fish/conf.d/*.fish ~/.config/fish/conf.d
+ln -sf ~/dotfiles/fish/functions/*.fish ~/.config/fish/functions
+ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
 
 printf "${BLUE}Finished.${NORMAL}\n"
