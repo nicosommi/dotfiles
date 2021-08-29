09023a19e40cfb5a0795ba647122d88394b11fba
nicosommi <nicosommi@gmail.com>
2021-08-29 16:27:35 +0200

    fix: sets go 1.17 for now

--- fish/conf.d/golang.fish
+++ fish/conf.d/golang.fish
@@ -1,9 +1,13 @@
 function load_goroot
   set -gx GOPATH ~/go
   set -gx GOROOT (go env GOROOT)
-
+  
   fish_add_path $GOPATH/bin
   fish_add_path $GOROOT/bin
 end
+
+set -gx GOROOT ~/homebrew/Cellar/go/1.17
+
+fish_add_path ~/homebrew/Cellar/go/1.17/bin
 
 load_goroot
